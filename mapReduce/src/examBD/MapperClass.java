b041f59a6d96d78a6fab0a70aae77b68887cf3af
Ferdis17 <fmuriyesu@users.noreply.github.com>
2018-08-19 23:33:21 -0400

    Mapper
    Mapper worked on

--- mapReduce/src/examBD/MapperClass.java
+++ mapReduce/src/examBD/MapperClass.java
@@ -13,10 +13,25 @@ import org.apache.hadoop.mapred.Reporter;
 public class MapperClass extends MapReduceBase implements Mapper<LongWritable, Text, CompositeKey, IntWritable>{
 
 	@Override
-	public void map(LongWritable arg0, Text arg1, OutputCollector<CompositeKey, IntWritable> arg2, Reporter arg3)
-			throws IOException {
-		// TODO Auto-generated method stub
+	public void map(LongWritable key, Text value,OutputCollector<CompositeKey, IntWritable> arg2, Reporter reporter)
+	throws IOException {
+		String row = value.toString();
 		
-	}
+		String[] cells = row.split("\t");
+		
+		String serverId = cells[4]; 
+		String time = cells[1];
+		int usage = Integer.parseInt(cells[2]);
+		CompositeKey cKey = null;
+		if (time >= 5 && time < 8) {
+			cKey = new CompositeKey(key, "Early");
+		}
+		// do the rest 
+		
+		output.collect(cKey, new IntWritable(usage));
+ }	  
+ 	 
+
+	
 
 }
